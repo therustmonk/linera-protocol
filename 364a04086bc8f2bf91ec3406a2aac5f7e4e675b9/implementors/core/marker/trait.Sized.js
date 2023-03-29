56b561bd5ee5a1e9476c9ad0647dd2c79c881160
jvff <jvff@users.noreply.github.com>
2023-03-29 19:51:30 +0000

    deploy: 94a74ee220da1c12d9836864f20bc2eddc29e8e1

--- 364a04086bc8f2bf91ec3406a2aac5f7e4e675b9/implementors/core/marker/trait.Sized.js
+++ 364a04086bc8f2bf91ec3406a2aac5f7e4e675b9/implementors/core/marker/trait.Sized.js
@@ -4,5 +4,6 @@
 "serde_json":[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"serde_json/value/struct.RawValue.html\" title=\"struct serde_json::value::RawValue\">RawValue</a>",1,["serde_json::raw::RawValue"]]],
 "wasmer_types":[],
 "wasmparser":[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"wasmparser/types/struct.KebabStr.html\" title=\"struct wasmparser::types::KebabStr\">KebabStr</a>",1,["wasmparser::validator::types::KebabStr"]]],
-"wasmtime_environ":[]
+"wasmtime_environ":[],
+"winnow":[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"winnow/stream/struct.Bytes.html\" title=\"struct winnow::stream::Bytes\">Bytes</a>",1,["winnow::stream::Bytes"]],["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"winnow/stream/struct.BStr.html\" title=\"struct winnow::stream::BStr\">BStr</a>",1,["winnow::stream::BStr"]]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
