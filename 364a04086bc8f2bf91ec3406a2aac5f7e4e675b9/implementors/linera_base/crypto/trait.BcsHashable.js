ca8d263c86010f7bd456e9e6f7423d34073a2449
afck <afck@users.noreply.github.com>
2023-05-28 20:35:18 +0000

    deploy: bece9a80aa5d890e5e52db5b57b2e07b950ea19d

--- 364a04086bc8f2bf91ec3406a2aac5f7e4e675b9/implementors/linera_base/crypto/trait.BcsHashable.js
+++ 364a04086bc8f2bf91ec3406a2aac5f7e4e675b9/implementors/linera_base/crypto/trait.BcsHashable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "linera_base":[],
-"linera_chain":[["impl <a class=\"trait\" href=\"linera_base/crypto/trait.BcsHashable.html\" title=\"trait linera_base::crypto::BcsHashable\">BcsHashable</a> for <a class=\"struct\" href=\"linera_chain/data_types/struct.Value.html\" title=\"struct linera_chain::data_types::Value\">Value</a>"]],
+"linera_chain":[["impl <a class=\"trait\" href=\"linera_base/crypto/trait.BcsHashable.html\" title=\"trait linera_base::crypto::BcsHashable\">BcsHashable</a> for <a class=\"enum\" href=\"linera_chain/data_types/enum.Value.html\" title=\"enum linera_chain::data_types::Value\">Value</a>"]],
 "linera_sdk":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
